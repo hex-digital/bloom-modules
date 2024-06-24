8856f9d19160121e814b4728de157280a1f1f755
Giuseppe <g@hexdigital.com>
2024-06-24 14:41:21 +0100

    feat: add module configs

---
+++ src/BaseImage/config/components.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'base.image' => Bloom\Modules\BaseImage\Components\BaseImage::class,
+];
---
+++ src/Buttons/config/components.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'buttons' => Bloom\Modules\Buttons\Components\Buttons::class,
+    'button' => Bloom\Modules\Buttons\Components\Button::class,
+];
