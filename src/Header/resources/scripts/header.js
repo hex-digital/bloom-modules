474ab71a3fea316fdbd3d5db07c2eed9fe003c14
Giuseppe <g@hexdigital.com>
2024-07-10 23:21:06 +0100

    doc(header): update code comments

--- src/Header/resources/scripts/header.js
+++ src/Header/resources/scripts/header.js
@@ -19,20 +19,20 @@ class Header {
     // Listen to mouseover or key.enter events on Main nav items. If item has subnav, then open it.
     navItems.forEach((navItem) => {
       navItem.addEventListener('mouseover', () => {
-        //As user is using mouse, assuming accesibility features aren't required, so disable
+        // As user is using mouse, assuming accesibility features aren't required, so disable
         this.state.accessibleMode = false;
         this.setActiveNav(navItem);
       });
       navItem.addEventListener('keydown', (event) => {
         if (event.key === 'Enter') {
-          //As user is using keyboard, assuming accesibility features are required, so enable
+          // As user is using keyboard, assuming accesibility features are required, so enable
           this.state.accessibleMode = true;
           this.setActiveNav(navItem, true);
         }
       });
     });
 
-    // Closes the subnav if open and the user presses the escape key
+    // If user presses the escape button on an open subnav, close it
     this.subnavContainer.addEventListener('keydown', (event) => {
       if (event.key === 'Escape') {
         this.header.querySelector(`[data-nav-id="${this.state.activeNav}"] button`).focus();
@@ -53,22 +53,22 @@ class Header {
   setActiveNav(navItem) {
 
     if (!navItem.dataset.hasSubnav) {
-      // If subnav is open and mouseover a normal nav link, call function to reset active navs
+      // If subnav is open and user mouses over a normal nav link, close subnav
       this.closeActiveNav();
     } else {
-      //Call function to set active nav and rerender frontend
+      // Set active nav and rerender frontend
       this.openActiveNav(navItem.dataset.navId);
     }
 
   }
 
   toggleSubnav(state) {
-    // subnav is changing to remove currently active subnav
+    // Subnav is changing, remove currently active subnav
     this.deactivateElement('c-header__subnav--active');
 
-    // opening the subnav
+    // Opening new subnav
     if (this.state.activeNav && state === 'open') {
-      // open subnav container and transition height to new
+      // Open subnav container and transition height to fit content
       this.subnavContainer.classList.add('c-header__subnav-container--active');
       this.subnavContainer.style.height = this.header.querySelector(`[data-subnav-id="${this.state.activeNav}"]`) ? `${this.header.querySelector(`[data-subnav-id="${this.state.activeNav}"]`).offsetHeight}px` : 0;
 
@@ -76,19 +76,19 @@ class Header {
       subnavItem.setAttribute('aria-expanded', 'true');
       subnavItem.classList.add('c-header__subnav--active');
 
-      //if accessibleMode (keyboard navigating), focus first item in subnav
+      // If accessibleMode (keyboard navigating), focus first item in subnav
       if (this.state.accessibleMode) {
         // A small delay is required, as the focus interrupts the dropdown of the subnav.
         setTimeout(() => {
           this.header.querySelector(`[data-subnav-id="${this.state.activeNav}"] > [tabindex="0"]:first-of-type`).focus();
         }, 250);
 
-        // Allows keyboard users to loop through items in a subnav. (Tabbing on last element takes you to first, shift + tab on first element takes you to last element within the subnav.)
+        // Allows keyboard users to loop through items in an active subnav. (Tabbing on last element takes you to first, shift + tab on first element takes you to last element.)
         this.setFocusTrap(subnavItem);
       }
     }
 
-    // closing the subnav
+    // Close the subnav
     if (state === 'close') {
       this.deactivateElement('c-header__subnav-container--active');
       this.deactivateElement('c-header__subnav--active');
@@ -107,7 +107,7 @@ class Header {
   }
 
   handleActiveNav(state) {
-    // active nav item is changing to remove currently active nav item
+    // Active nav item is changing, remove currently active nav item
     this.deactivateElement('c-header__nav-item--active');
 
     if (this.state.activeNav && state === 'open') {
@@ -117,12 +117,12 @@ class Header {
     }
 
     if (state === 'close') {
-      // completely closing nav items
+      // Remove currently active nav item
       this.deactivateElement('c-header__nav-item--active');
     }
   }
 
-  // finds the element with the active class passed as the parameter, removes the active class and toggles the aria-expanded attribute if required.
+  // Finds the element with the active class passed as the parameter, removes the active class and toggles the aria-expanded attribute if required.
   deactivateElement(element) {
     const targetElement = this.header.querySelector(`.${element}`)
 
@@ -136,24 +136,23 @@ class Header {
   }
 
   setFocusTrap(activeSubnav) {
-    // check activeSubnav is indeed active
+    // Check activeSubnav is active
     if (activeSubnav.classList.contains('c-header__subnav--active')) {
-      //get first and last focusable Elements
+      // Get first and last focusable Elements
       const firstFocusableElement = activeSubnav.querySelector('[tabindex="0"]:first-child');
       const lastFocusableElement = activeSubnav.querySelector('[tabindex="0"]:last-child');
 
 
-      // event listener on active subnav
       activeSubnav.addEventListener('keydown', (event) => {
         if (event.key === 'Tab') {
           if (event.shiftKey) { // Shift + Tab
-            // if focused element is firstFocusableElement, and user has shift + Tab, focus last element in list
+            // If focused element is firstFocusableElement, and user has pressed Shift + Tab, focus last element in list
             if (document.activeElement === firstFocusableElement) {
               lastFocusableElement.focus();
               event.preventDefault();
             }
           } else { // Tab
-            // if focused element is lastFocusableElement, focus first element in list
+            // If focused element is lastFocusableElement, focus first element in list
             if (document.activeElement === lastFocusableElement) {
               firstFocusableElement.focus();
               event.preventDefault();
@@ -165,7 +164,7 @@ class Header {
 
   }
 
-  // handles updating the frontend
+  // Handles updating the frontend
   render(action) {
     this.handleActiveNav(action);
     this.toggleSubnav(action);
